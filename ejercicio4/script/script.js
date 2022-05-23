2e354ef6be79aa740469c7c9bb15895acd9b274a
David Alejo <davidalejo@MacBook-Pro-de-David-2.local>
2022-05-23 11:51:08 +0200

    segundo commit con ejercicio 4 y 6 terminado

--- ejercicio4/script/script.js
+++ ejercicio4/script/script.js
@@ -1,1 +1,39 @@
 'use strict';
+
+let result = [];
+let sum;
+
+//Función para contar el número de veces que se repite un número dentro del array
+function numRepet(result, num){
+
+    return result.filter(element => element === num).length;
+}
+
+/* 
+* Función que simula la tirada de 2 dados, el resultado lo introduce en un array e imprime por pantalla 
+* el numero de veces que se repite
+*/
+function throwDice(numThrow){
+    for (let i =0; i < numThrow; i++){
+        let dice1 = Math.floor(Math.random()*(5+1)+1);
+        let dice2 = Math.floor(Math.random()*(5+1)+1);
+        //Suma de los dos dados
+        sum= dice1+dice2;
+        //Introducimos el valor en el array
+        result.push(sum);
+    }
+    
+    document.write("El 2 ha salido " + numRepet(result,2)+ " veces. <br>");
+    document.write("El 3 ha salido " + numRepet(result,3)+ " veces. <br>");
+    document.write("El 4 ha salido " + numRepet(result,4)+ " veces. <br>");
+    document.write("El 5 ha salido " + numRepet(result,5)+ " veces. <br>");
+    document.write("El 6 ha salido " + numRepet(result,6)+ " veces. <br>");
+    document.write("El 7 ha salido " + numRepet(result,7)+ " veces. <br>");
+    document.write("El 8 ha salido " + numRepet(result,8)+ " veces. <br>");
+    document.write("El 9 ha salido " + numRepet(result,9)+ " veces. <br>");
+    document.write("El 10 ha salido " + numRepet(result,10)+ " veces. <br>");
+    document.write("El 11 ha salido " + numRepet(result,11)+ " veces. <br>");
+    document.write("El 12 ha salido " + numRepet(result,12)+ " veces. <br>");
+}
+
+throwDice(36000);
--- ejercicio6/script/script.js
+++ ejercicio6/script/script.js
@@ -8,7 +8,7 @@ function Persona(name,age,genere){
     
     //Método obtDetalles para mostrar por pantalla el nombre, la edad y el género de la persona
     this.obtDetalles=function(){
-        return document.write("Nombre: " +this.name+"\n Edad: "+this.age+"\n Género: "+this.genere);
+        return document.write("Nombre: " +this.name+"\n Edad: "+this.age+"\n Género: "+this.genere+"<br>");
     }
 }
 
@@ -17,13 +17,13 @@ function Persona(name,age,genere){
 let Estudiante = function(name,age,genere,course,group){
     
     //Llama al padre Persona
-    Persona.call(name,age,genere);
+    Persona.call(this,name,age,genere);
     this.course = course;
     this.group = group;
 
     //Método registrar
     this.registrar=function(){
-        return document.write("El alumno "+this.name+ " de "+this.course+" "+this.group+" ha sido registrado.");
+        return document.write("El alumno "+this.name+ " de "+this.course+" "+this.group+" ha sido registrado."+"<br>");
     }
 }
 
@@ -32,13 +32,13 @@ let Estudiante = function(name,age,genere,course,group){
 let Profesor = function(name,age,genere,module,level){
     
     //Llama al padre Persona
-    Persona.call(name,age,genere);
+    Persona.call(this,name,age,genere);
     this.module = module;
     this.level = level;
 
     //Método asignar
     this.asignar=function(){
-        return document.write("El profesor "+this.name+" ha sido asignado en el módulo de "+this.module);
+        return document.write("El profesor "+this.name+" ha sido asignado en el módulo de "+this.module+"<br>");
     }
 }
 
@@ -47,4 +47,8 @@ let persona1 = new Persona("Pepe", 50 ,"hombre");
 persona1.obtDetalles();
 
 let persona2 = new Estudiante("David",34,"hombre","2º","B");
-persona2.obtDetalles();
+persona2.registrar();
+
+let persona3 = new Profesor("Cristina",38,"mujer","DWEC","Segundo");
+persona3.obtDetalles();
+persona3.asignar();
